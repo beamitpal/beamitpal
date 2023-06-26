6b2f5c2784f9950af751041c39365cf4d690c81b
Amit Pal <104966501+beamitpal@users.noreply.github.com>
2023-06-26 18:15:45 +0530

    portfolio
    portfolio

---
+++ tailwind.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          brand: '#001122',
+          light: '#EEEEEE',
+          dark: '#080202',
+        }
+      }
+    }
+  }
